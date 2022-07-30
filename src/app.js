7b32a2ed64b0a8fdc3e3bf7b8aa20d77337bb235
Daniela Passos <moitidani@gmail.com>
2022-07-30 22:11:07 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -49,10 +49,17 @@ function displayForecast(){
   `;
   });
 
-    
+  forecastHTML = forecastHTML + `</div>`;
+
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates){
+  let apiKey = "c0319fc534d112644590c2ba188b9555";
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
+
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -63,7 +70,7 @@ function displayTemperature(response) {
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
 
-  displayForecast();
+ 
 
   celsiusTemperature = response.data.main.temp;
 
@@ -78,7 +85,14 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+
+  getForecast(response.data.coord)
+
 }
+
+
+
 
 function search(city) {
   let apiKey = "c0319fc534d112644590c2ba188b9555";
