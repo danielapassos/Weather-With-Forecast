d7027b46adb89b648c676583f2af6660d1d72e96
Daniela Passos <moitidani@gmail.com>
2022-07-30 11:51:44 +0300

    Updated APIkey

--- src/app.js
+++ src/app.js
@@ -47,7 +47,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "c0319fc534d112644590c2ba188b9555";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
